0477df0e19f87da9643ce9a111b3cf1f1b31632d
Kwon-Seungmin <tmdals3320@naver.com>
2023-10-12 01:08:33 +0900

    Feat: 메인 페이지 추가
    [#3] 메인페이지 구조 개발 작업 수행
    최소한의 구분만을 위해 css를 작업 했고, 추후 상세 디자인에 따라 수정 요함.

---
+++ src/app/(main)/component/contents/Thumbnail.tsx
@@ -0,0 +1,9 @@
+export default function Thumbnail() {
+    return (
+        <>
+            <div className="thumbnail">
+                썸네일 들어가요
+            </div></>
+    )
+
+}
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import SimpleSearch from "./component/contents/SimpleSearch";
+import Thumbnail from "./component/contents/Thumbnail";
+
+export default function Home() {
+  return (
+    <>
+        <SimpleSearch />
+        <Thumbnail />
+    </>
+  )
+}
