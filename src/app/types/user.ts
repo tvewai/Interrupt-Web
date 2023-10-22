2037bc4f97cfd160f3e7284b42d16e7952054455
seungmin-kr <tmdals3320@naver.com>
2023-10-22 23:57:46 +0900

    Feat: 회원가입 요청 파라미터 타입 추가

---
+++ src/app/types/user.ts
@@ -0,0 +1,7 @@
+export type SignUpRequestType = {
+    loginId: string | undefined,
+    password: string | undefined,
+    name: string | undefined,
+    email: string | undefined,
+    emailVerifyCodeKey: string | undefined,
+}
